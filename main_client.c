b723904756e77261a466bc28c251cd62798626d0
Nick Viola <nickvswims@hotmail.com>
2026-05-11 11:27:20 -0400

    Complete Checkpoint 1: all 5 requirements done

--- main_client.c
+++ main_client.c
@@ -10,23 +10,72 @@
 #include <pthread.h>
 
 #define PORT_NUM 8080
+#define MAX_USERS 32
 
 void error(const char *msg)
 {
-	perror(msg);
-	exit(0);
+    perror(msg);
+    exit(0);
+}
+
+/* Req 5: ANSI color codes */
+const char* COLORS[] = {
+    "\033[31m",   /* red */
+    "\033[32m",   /* green */
+    "\033[33m",   /* yellow */
+    "\033[34m",   /* blue */
+    "\033[35m",   /* magenta */
+    "\033[36m",   /* cyan */
+    "\033[91m",   /* bright red */
+    "\033[92m",   /* bright green */
+    "\033[93m",   /* bright yellow */
+    "\033[94m",   /* bright blue */
+    "\033[95m",   /* bright magenta */
+    "\033[96m",   /* bright cyan */
+};
+#define NUM_COLORS 12
+#define RESET "\033[0m"
+
+/* Req 5: map usernames to colors */
+char color_names[MAX_USERS][64];
+int color_assigned[MAX_USERS];
+int num_colors_used = 0;
+pthread_mutex_t color_lock = PTHREAD_MUTEX_INITIALIZER;
+
+const char* get_color(const char* name) {
+    pthread_mutex_lock(&color_lock);
+
+    for (int i = 0; i < num_colors_used; i++) {
+        if (strcmp(color_names[i], name) == 0) {
+            int idx = color_assigned[i];
+            pthread_mutex_unlock(&color_lock);
+            return COLORS[idx];
+        }
+    }
+
+    if (num_colors_used < MAX_USERS) {
+        strncpy(color_names[num_colors_used], name, 64);
+        color_assigned[num_colors_used] = num_colors_used % NUM_COLORS;
+        const char* c = COLORS[num_colors_used % NUM_COLORS];
+        num_colors_used++;
+        pthread_mutex_unlock(&color_lock);
+        return c;
+    }
+
+    pthread_mutex_unlock(&color_lock);
+    return RESET;
 }
 
 typedef struct _ThreadArgs {
-	int clisockfd;
+    int clisockfd;
 } ThreadArgs;
 
 void* thread_main_receive(void* args)
 {
-	pthread_detach(pthread_self());
+    pthread_detach(pthread_self());
 
-	int sockfd = ((ThreadArgs*) args)->clisockfd;
-	free(args);
+    int sockfd = ((ThreadArgs*) args)->clisockfd;
+    free(args);
 
     char buffer[512];
     int nrcv;
@@ -34,19 +83,38 @@ void* thread_main_receive(void* args)
     while (1) {
         memset(buffer, 0, 512);
         nrcv = recv(sockfd, buffer, 511, 0);
-        if (nrcv <= 0) { break; }
+        if (nrcv <= 0) break;
         buffer[nrcv] = '\0';
-        printf("\n%s\n", buffer);
-	}
+
+        /* Req 5: parse sender name from "[Name (IP)] message" and colorize */
+        if (buffer[0] == '[') {
+            char name[64] = {0};
+            char* start = buffer + 1;
+            char* end = strstr(start, " (");
+            if (end != NULL) {
+                int len = end - start;
+                if (len > 63) len = 63;
+                strncpy(name, start, len);
+                const char* color = get_color(name);
+                printf("\n%s%s%s\n", color, buffer, RESET);
+            } else {
+                printf("\n%s\n", buffer);
+            }
+        } else {
+            /* system messages (join/leave) in default color */
+            printf("\n%s\n", buffer);
+        }
+        fflush(stdout);
+    }
 
     return NULL;
 }
 
 void* thread_main_send(void* args) {
-	pthread_detach(pthread_self());
+    pthread_detach(pthread_self());
 
-	int sockfd = ((ThreadArgs*) args)->clisockfd;
-	free(args);
+    int sockfd = ((ThreadArgs*) args)->clisockfd;
+    free(args);
 
     char buffer[256];
     int nsen;
@@ -55,13 +123,11 @@ void* thread_main_send(void* args) {
         memset(buffer, 0, 256);
         fgets(buffer, 255, stdin);
 
-        if (strlen(buffer) == 1) { break; }
+        if (strlen(buffer) == 1) break;
         buffer[strcspn(buffer, "\n")] = '\0';
 
         nsen = send(sockfd, buffer, strlen(buffer), 0);
-        if (nsen < 0) {
-            error("ERROR writing to socket");
-        }
+        if (nsen < 0) error("ERROR writing to socket");
     }
 
     return NULL;
@@ -71,30 +137,32 @@ int main(int argc, char *argv[])
 {
     if (argc < 2) error("Please specify hostname");
 
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) error("ERROR opening socket");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
 
-	struct sockaddr_in serv_addr;
-	socklen_t slen = sizeof(serv_addr);
-	memset((char*) &serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(PORT_NUM);
+    struct sockaddr_in serv_addr;
+    socklen_t slen = sizeof(serv_addr);
+    memset((char*) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(PORT_NUM);
 
     printf("Trying connecting to %s\n", inet_ntoa(serv_addr.sin_addr));
 
-    if (connect(sockfd, (struct sockaddr*) &serv_addr, slen) < 0) {
+    if (connect(sockfd, (struct sockaddr*) &serv_addr, slen) < 0)
         error("ERROR connecting");
-    }
 
     printf("Connected to server\n");
 
-    /* Req 3: prompt for username and send to server before spawning threads */
+    /* Req 3: prompt for username and send to server */
     char username[64];
     printf("Type your user name: ");
     fflush(stdout);
-    fgets(username, 64, stdin);
-    username[strcspn(username, "\n")] = '\0';
+    /* scanf reads the name and stops before the newline */
+    scanf("%63s", username);
+    /* fix: drain the leftover newline so thread_main_send doesn't read it */
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
     send(sockfd, username, strlen(username), 0);
 
     pthread_t tid1;
@@ -109,9 +177,8 @@ int main(int argc, char *argv[])
     args->clisockfd = sockfd;
     pthread_create(&tid2, NULL, thread_main_receive, (void*) args);
 
-    /* wait for send thread to finish */
     pthread_join(tid1, NULL);
 
     close(sockfd);
-	return 0;
+    return 0;
 }
--- main_server.c
+++ main_server.c
@@ -12,8 +12,8 @@
 
 void error(const char *msg)
 {
-	perror(msg);
-	exit(1);
+    perror(msg);
+    exit(1);
 }
 
 /* represent each connected client */
@@ -27,7 +27,6 @@ typedef struct _USR {
 USR* head = NULL;
 pthread_mutex_t list_lock = PTHREAD_MUTEX_INITIALIZER;
 
-/* add a new client to the list */
 void add_client(int clisockfd, char* ip) {
     pthread_mutex_lock(&list_lock);
     USR* newuser = (USR*) malloc(sizeof(USR));
@@ -40,64 +39,47 @@ void add_client(int clisockfd, char* ip) {
         head = newuser;
     } else {
         USR* cur = head;
-        while (cur->next != NULL) {
-            cur = cur->next;
-        }
+        while (cur->next != NULL) cur = cur->next;
         cur->next = newuser;
     }
-
     pthread_mutex_unlock(&list_lock);
 }
 
 void remove_client(int clisockfd) {
     pthread_mutex_lock(&list_lock);
-
     USR* cur = head;
     USR* prev = NULL;
 
     while (cur != NULL) {
         if (cur->clisockfd == clisockfd) {
-            if (prev == NULL) {
-                head = cur->next;
-            } else {
-                prev->next = cur->next;
-            }
+            if (prev == NULL) head = cur->next;
+            else prev->next = cur->next;
             free(cur);
             break;
         }
         prev = cur;
         cur = cur->next;
     }
-
     pthread_mutex_unlock(&list_lock);
 }
 
-/* print the current list of clients */
 void print_client_list() {
     printf("Connected Clients:\n");
     USR* cur = head;
-
-    if (cur == NULL) {
-        printf("No clients connected\n");
-    }
-
+    if (cur == NULL) printf("No clients connected\n");
     while (cur != NULL) {
         printf(" %s (%s)\n", cur->user, cur->ip);
         cur = cur->next;
     }
 }
 
-/* send a message to all connected clients except sender */
 void broadcast(int fromfd, char* message) {
     pthread_mutex_lock(&list_lock);
-
     USR* cur = head;
     while (cur != NULL) {
         if (cur->clisockfd != fromfd) {
             int nsen = send(cur->clisockfd, message, strlen(message), 0);
-            if (nsen < 0) {
-                error("ERROR send() failed");
-            }
+            if (nsen < 0) error("ERROR send() failed");
         }
         cur = cur->next;
     }
@@ -105,15 +87,15 @@ void broadcast(int fromfd, char* message) {
 }
 
 typedef struct _ThreadArgs {
-	int clisockfd;
+    int clisockfd;
 } ThreadArgs;
 
 void* thread_main(void* args)
 {
-	pthread_detach(pthread_self());
+    pthread_detach(pthread_self());
 
-	int clisockfd = ((ThreadArgs*) args)->clisockfd;
-	free(args);
+    int clisockfd = ((ThreadArgs*) args)->clisockfd;
+    free(args);
 
     /* get client IP address */
     struct sockaddr_in cliaddr;
@@ -122,7 +104,6 @@ void* thread_main(void* args)
     char ip[32];
     strncpy(ip, inet_ntoa(cliaddr.sin_addr), 32);
 
-    /* add client to list */
     add_client(clisockfd, ip);
 
     /* Req 3: first message from client is their username */
@@ -130,8 +111,7 @@ void* thread_main(void* args)
     int nrcv = recv(clisockfd, username, 63, 0);
     if (nrcv > 0) {
         username[nrcv] = '\0';
-
-        /* store username in the client list */
+        /* store in list */
         pthread_mutex_lock(&list_lock);
         USR* cur = head;
         while (cur != NULL) {
@@ -148,28 +128,26 @@ void* thread_main(void* args)
 
     /* Req 4: broadcast join notification */
     char join_msg[128];
-    snprintf(join_msg, sizeof(join_msg), "%s (%s) joined the chat room!", username, ip);
+    snprintf(join_msg, sizeof(join_msg), "%s (%s) joined the chat room!\n", username, ip);
     broadcast(clisockfd, join_msg);
 
     /* receive and broadcast messages */
-	char buffer[256];
+    char buffer[256];
+    /* fix: increase formatted buffer to safely hold username+ip+message */
+    char formatted[400];
 
     while ((nrcv = recv(clisockfd, buffer, 255, 0)) > 0) {
         buffer[nrcv] = '\0';
-
-        /* format message with sender name and IP */
-        char formatted[320];
-        snprintf(formatted, sizeof(formatted), "[%s (%s)] %s", username, ip, buffer);
+        snprintf(formatted, sizeof(formatted), "[%s (%s)] %s\n", username, ip, buffer);
         broadcast(clisockfd, formatted);
         memset(buffer, 0, 256);
-	}
+    }
 
-    /* Req 4: broadcast leave notification before removing */
+    /* Req 4: broadcast leave notification */
     char leave_msg[128];
-    snprintf(leave_msg, sizeof(leave_msg), "%s (%s) left the room!", username, ip);
+    snprintf(leave_msg, sizeof(leave_msg), "%s (%s) left the room!\n", username, ip);
     broadcast(clisockfd, leave_msg);
 
-    /* client disconnect */
     remove_client(clisockfd);
     print_client_list();
     close(clisockfd);
@@ -179,39 +157,38 @@ void* thread_main(void* args)
 
 int main(int argc, char *argv[])
 {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) error("ERROR opening socket");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
 
-	struct sockaddr_in serv_addr;
-	socklen_t slen = sizeof(serv_addr);
-	memset((char*) &serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;	
-	serv_addr.sin_port = htons(PORT_NUM);
+    struct sockaddr_in serv_addr;
+    socklen_t slen = sizeof(serv_addr);
+    memset((char*) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT_NUM);
 
-    if (bind(sockfd, (struct sockaddr*) &serv_addr, slen) < 0) {
+    if (bind(sockfd, (struct sockaddr*) &serv_addr, slen) < 0)
         error("ERROR on binding");
-    }
 
     listen(sockfd, 5);
     printf("Server started on port %d\n", PORT_NUM);
 
     while (1) {
         struct sockaddr_in cli_addr;
-		socklen_t clen = sizeof(cli_addr);
-		int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clen);
-		if (newsockfd < 0) error("ERROR on accept");
+        socklen_t clen = sizeof(cli_addr);
+        int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clen);
+        if (newsockfd < 0) error("ERROR on accept");
 
-		printf("Connected: %s\n", inet_ntoa(cli_addr.sin_addr));
+        printf("Connected: %s\n", inet_ntoa(cli_addr.sin_addr));
 
-		ThreadArgs* args = (ThreadArgs*) malloc(sizeof(ThreadArgs));
-		if (args == NULL) error("ERROR creating thread argument");
-		args->clisockfd = newsockfd;
+        ThreadArgs* args = (ThreadArgs*) malloc(sizeof(ThreadArgs));
+        if (args == NULL) error("ERROR creating thread argument");
+        args->clisockfd = newsockfd;
 
-		pthread_t tid;
-		if (pthread_create(&tid, NULL, thread_main, (void*) args) != 0)
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, thread_main, (void*) args) != 0)
             error("ERROR creating a new thread");
-	}
+    }
 
-	return 0;
+    return 0;
 }
